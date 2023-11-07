bce15a4dca92292a0601d1d8b7102daaaf4bff71
AzzaBenRomdhan <azza.benromdhan@esprit.tn>
2023-11-07 23:24:31 +0100

    first commit

---
+++ app/src/main/java/org/esprit/registrationform/MainActivity.java
@@ -0,0 +1,25 @@
+package org.esprit.registrationform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+   /* public void onclickRegister (View view) {
+        EditText FirstName = findViewById(R.id.FirstNameInput);
+        EditText LastName = findViewById(R.id.LastNameInput);
+        TextView txtFirstname = findViewById(R.id.txtFirstname);
+        TextView txtLastName =  findViewById(R.id.txtLastName);
+        txtFirstname.setText("FirstName: "+ FirstName.getText().toString());
+        txtLastName.setText("LastName: " + LastName.getText().toString());
+    }*/
+}
