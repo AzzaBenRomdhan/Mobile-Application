cc1310f1f68e4c409e9cb861ddaffda46d7c3158
ASUS <salma.khemiri@esprit.tn>
2023-11-09 00:00:46 +0100

    controle de saisie sur mail + msg de succes

--- app/src/main/java/org/esprit/registrationform/DAO/UserDAO.java
+++ app/src/main/java/org/esprit/registrationform/DAO/UserDAO.java
@@ -1,23 +1,18 @@
 package org.esprit.registrationform.DAO;
 
 import androidx.room.Dao;
-import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
-import androidx.room.Update;
 
 import org.esprit.registrationform.entities.User;
 
-import org.esprit.registrationform.entities.User;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Dao
 public interface UserDAO {
 
     @Insert
-    public void addUser (User u );
+    public long addUser (User u );
 
     @Query("SELECT * FROM User")
     List<User> getAll();
--- app/src/main/java/org/esprit/registrationform/Login2.java
+++ app/src/main/java/org/esprit/registrationform/Login2.java
@@ -2,23 +2,36 @@ package org.esprit.registrationform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login2 extends AppCompatActivity {
     TextView tv ;
+    Button btn_shop_product;
+
 
     SharedPreferences shared ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
-        tv= findViewById(R.id.textView0);
+        tv= findViewById(R.id.email);
         shared = getSharedPreferences(Login.PREF,MODE_PRIVATE);
 
-        tv.setText(shared.getString("email","0000000000000000000000"));
+        tv.setText(shared.getString("EMAIL","5555"));
 
-
+        //redirection lil page products
+        btn_shop_product = findViewById(R.id.btn_shop_product);
+        btn_shop_product.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login2.this, itemsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
