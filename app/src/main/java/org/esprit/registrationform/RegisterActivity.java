c8eb7236f32217e250edb7c52992c73e6e5c3e75
ASUS <salma.khemiri@esprit.tn>
2023-11-09 18:52:33 +0100

    hi user name fixed e

---
+++ app/src/main/java/org/esprit/registrationform/RegisterActivity.java
@@ -0,0 +1,80 @@
+package org.esprit.registrationform;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText username, email, password, repassword;
+    Button signup, signin;
+    MyDataBaseHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.mail);
+        password = findViewById(R.id.password);
+        repassword = findViewById(R.id.repassword);
+        signup = findViewById(R.id.btnsignup);
+        signin = findViewById(R.id.btnsignin);
+        DB = new MyDataBaseHelper(this);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = username.getText().toString();
+                String userEmail = email.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if (user.equals("") || userEmail.equals("") || pass.equals("") || repass.equals("")) {
+                    Toast.makeText(RegisterActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Add email validation
+                    if (!Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
+                        Toast.makeText(RegisterActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Add password length validation
+                        if (pass.length() < 8) {
+                            Toast.makeText(RegisterActivity.this, "Password must be at least 8 characters long", Toast.LENGTH_SHORT).show();
+                        } else if (pass.equals(repass)) {
+                            Boolean checkuser = DB.checkusername(user);
+                            if (checkuser == false) {
+                                Boolean insert = DB.insertData(user, pass);
+                                if (insert == true) {
+                                    Toast.makeText(RegisterActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(RegisterActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(RegisterActivity.this, "User already exists! Please sign in", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Passwords not matching", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
