c57879df2fdd83e7990020c78e91e81a6961ff97
ASUS <salma.khemiri@esprit.tn>
2023-11-09 16:38:08 +0100

    login + register sans controle de saisie

--- app/src/main/java/org/esprit/registrationform/HomeActivity.java
+++ app/src/main/java/org/esprit/registrationform/HomeActivity.java
@@ -9,27 +9,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Login2 extends AppCompatActivity {
-    TextView tv ;
+public class HomeActivity extends AppCompatActivity {
+  //  TextView tv ;
     Button btn_shop_product;
 
 
-    SharedPreferences shared ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login2);
-        tv= findViewById(R.id.email);
-        shared = getSharedPreferences(Login.PREF,MODE_PRIVATE);
+        setContentView(R.layout.home);
+      //  tv= findViewById(R.id.email);
+       // shared = getSharedPreferences(Login.PREF,MODE_PRIVATE);
 
-        tv.setText(shared.getString("EMAIL","5555"));
+
+     //   tv.setText(shared.getString("EMAIL","5555"));
 
         //redirection lil page products
         btn_shop_product = findViewById(R.id.btn_shop_product);
         btn_shop_product.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Login2.this, itemsActivity.class);
+                Intent intent = new Intent(HomeActivity.this, itemsActivity.class);
                 startActivity(intent);
             }
         });
