2951cd58ec90ebd769da2604bed3d991358218d4
SirineAmri <73968181+SirineAmri@users.noreply.github.com>
2023-11-14 00:41:36 +0100

    sms

--- app/src/main/java/org/esprit/registrationform/AddCommentActivity.java
+++ app/src/main/java/org/esprit/registrationform/AddCommentActivity.java
@@ -21,6 +21,7 @@ public class AddCommentActivity extends AppCompatActivity {
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
    // private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
+    // private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
 
     private EditText editTextComment;
     private Button buttonAddComment;
